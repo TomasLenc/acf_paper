baa42134cb118881d4866f5fe0df5f30be14edf6
Tomas Lenc <tomas.lenc@gmail.com>
2023-02-24 14:11:30 +0100

    add main script that runs everything

---
+++ main.m
@@ -0,0 +1,26 @@
+addpath(genpath('lib'))
+
+main03_ir('ir_type', 'square');
+close all
+main03_ir('ir_type', 'erp');
+close all
+
+main04_snr;
+close all
+
+main05_emph('ir_type', 'square');
+close all
+main05_emph('ir_type', 'erp2');
+close all
+
+main06_scale;
+close all
+
+main07_shift;
+close all
+
+main08_coch;
+close all
+
+main09_eeg;
+close all
