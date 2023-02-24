ac8828cd62143ffe80d72cb990bf6cdf70f7bd25
Tomas Lenc <tomas.lenc@gmail.com>
2023-02-24 14:09:49 +0100

    add plot option ytick at each mean

--- lib/plot_multiple_cond.m
+++ lib/plot_multiple_cond.m
@@ -4,18 +4,24 @@ parser = inputParser;
 
 addParameter(parser, 'ax',  []); 
 addParameter(parser, 'plot_legend',  true); 
+addParameter(parser, 'ytick_at_means',  false); 
 addParameter(parser, 'ylim_quantile_cutoff',  0); % between 0 and 1
+addParameter(parser, 'prec',  3); % number decimal places 
 
 addParameter(parser, 'feat',  []); 
 addParameter(parser, 'feat_orig',  []); 
+addParameter(parser, 'feat_thr',  []); 
 
 parse(parser, varargin{:});
 
 ax = parser.Results.ax; 
 plot_legend = parser.Results.plot_legend; 
+ytick_at_means = parser.Results.ytick_at_means; 
+prec = parser.Results.prec; 
 
 feat = parser.Results.feat; 
 feat_orig = parser.Results.feat_orig; 
+feat_thr = parser.Results.feat_thr; 
 ylim_quantile_cutoff = parser.Results.ylim_quantile_cutoff; 
 
 
@@ -36,6 +42,15 @@ else
 end
 
 hold(ax, 'on'); 
+
+if ~isempty(feat_thr)
+    
+    plot(ax, [0, n_cond+1], [feat_thr.data, feat_thr.data], ...
+         '-', 'color', 'red', 'linew', 3)
+    ylims(1) = feat_thr.data - abs(feat_thr.data)*0.1;
+    ylims(2) = feat_thr.data + abs(feat_thr.data)*0.1;
+    
+end
 
 if length(feat_orig) == 1
     
@@ -58,6 +73,8 @@ elseif length(feat_orig) == length(feat)
 end
 
 h = []; 
+means = nan(1, n_cond);
+
 if ~isempty(feat)
     for i_cond=1:n_cond
         h(i_cond) = plot_points(ax, i_cond, feat(i_cond).data, ...
@@ -67,6 +84,7 @@ if ~isempty(feat)
         ylims(1) = min(quantile(feat(i_cond).data, ylim_quantile_cutoff), ylims(1)); 
         ylims(2) = max(quantile(feat(i_cond).data, 1-ylim_quantile_cutoff), ylims(2)); 
 
+        means(i_cond) = mean(feat(i_cond).data);
     end
 
     ax.XLim = [0.5, n_cond+0.5]; 
@@ -75,8 +93,12 @@ if ~isempty(feat)
     end
     ax.XAxis.Visible = 'off';  
     ax.TickDir = 'out'; 
-    ax.YTick = [ax.YTick(1), ax.YTick(end)];
-
+    if ytick_at_means
+        ax.YTick = round(sort(means), prec);
+    else
+        ax.YTick = [ax.YTick(1), ax.YTick(end)];
+    end
+    
     if plot_legend
         leg = legend(h, {feat.name}); 
         leg.Box = 'off'; 
