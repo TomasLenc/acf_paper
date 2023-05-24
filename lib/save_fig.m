8c9a63136768573e94b4da3676259d7b4e2d8ea6
Tomas Lenc <tomas.lenc@gmail.com>
2023-05-24 09:59:41 +0200

    only save svg figures

--- lib/save_fig.m
+++ lib/save_fig.m
@@ -7,7 +7,7 @@ if ismember(fname(end-3:end), {'.png', '.eps', '.svg', '.fig'})
     fname = fname(1:end-4);
 end
 
-saveas(f, fullfile(par.fig_path, [fname, '.fig']));  
+% saveas(f, fullfile(par.fig_path, [fname, '.fig']));  
 % print(fullfile(par.fig_path, [fname, '.png']), '-dpng', '-painters', f);  
 print(fullfile(par.fig_path, [fname, '.svg']), '-dsvg', '-painters', f);  
 
