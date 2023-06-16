ddd528fd326c33198a3b617196c76858f987a900
Tomas Lenc <tomas.lenc@gmail.com>
2023-06-16 11:13:04 +0200

    wip R

--- analysis.R
+++ analysis.R
@@ -22,26 +22,20 @@ for (i_data_dir in c(1:length(data_dirs))) {
         dir.create(fig_path)
     }
     
-    # Only noise
-    results_fname <- sprintf('report_noise')
-    
-    rmarkdown::render(sprintf('report_only_noise.Rmd'), 
-                      output_file=sprintf('%s/%s.html', data_path, results_fname))
-    
-    # Syncrange
-    results_fname <- sprintf('report_syncrange')
-    rmarkdown::render(sprintf('report_syncrange.Rmd'), 
-                      output_file=sprintf('%s/%s.html', data_path, results_fname))
+    # # Noise
+    # results_fname <- sprintf('report_noise')
+    # rmarkdown::render(sprintf('report_noise.Rmd'), 
+    #                   output_file=sprintf('%s/%s.html', data_path, results_fname))
+    # 
+    # # Syncrange
+    # results_fname <- sprintf('report_syncrange')
+    # rmarkdown::render(sprintf('report_syncrange.Rmd'), 
+    #                   output_file=sprintf('%s/%s.html', data_path, results_fname))
     
     # N-lags vs. SNR
-    ir_type <- 'square'
-    results_fname <- sprintf('report_irType-square_nalgsVsSnr')
+    results_fname <- sprintf('report_nalgsVsSnr')
     rmarkdown::render(sprintf('report_nlags_nfrex.Rmd'), 
                       output_file=sprintf('%s/%s.html', data_path, results_fname))
 
-    ir_type <- 'erp2'
-    results_fname <- sprintf('report_irType-erp2_nalgsVsSnr')
-    rmarkdown::render(sprintf('report_nlags_nfrex.Rmd'), 
-                      output_file=sprintf('%s/%s.html', data_path, results_fname))
     
 }
