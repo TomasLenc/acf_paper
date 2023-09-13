195b690ae69626bea63255e0e8fb0bd68a38a126
Tomas Lenc <tomas.lenc@gmail.com>
2023-09-13 17:26:19 +0200

    set up to run on erp2 kernel

--- main.m
+++ main.m
@@ -30,15 +30,18 @@ end
 
 %%
 
-% % this one is independent on the frex/lags selection 
-% par = get_par(); 
-% 
-% par.n_rep = 50; 
-% 
-% par.data_path = fullfile(par.data_path); 
-% 
-% main_snr_vs_nlags(par,...
-%     'prepared_noise', noise_all_samples); 
+% this one is independent on the frex/lags selection 
+par = get_par(); 
+
+par.ir_type = 'erp2'; 
+par.ir = get_ir(par.ir_type, par.fs); 
+
+par.n_rep = 50; 
+
+par.data_path = fullfile(par.data_path); 
+
+main_snr_vs_nlags(par,...
+    'prepared_noise', noise_all_samples); 
 
 
 %% 
@@ -79,62 +82,62 @@ run_mains
 
 %% 
 
-% % This one is only to double check that the better robustnesss of ACF to noise
-% % is not simply because we're taking more values (lags of interest). So let's
-% % equalize the number of frex and lags of interest and re-run only the relevant
-% % scripts. 
-% 
-% sel_name = 'maxlag-11lags_meterRel-0.8_meterUnrel-0.6_1.0_1.4'; 
-% 
-% par = get_par(); 
-% 
-% par.data_path = fullfile(par.data_path, sel_name); 
-% par.fig_path = par.data_path; 
-% mkdir(par.data_path); 
-% 
-% % frequencies of interst
-% par.max_freq = 5; 
-% par.max_freq_plot = 5.1; 
-% par.f0_to_excl = 5; 
-% [par.freq_meter_rel, par.freq_meter_unrel, par.frex] = get_meter_freq(...
-%                                                 par.max_freq, ...
-%                                                 'f0_to_excl', par.f0_to_excl);
-% 
-% % lags of interest 
-% par.max_lag = par.trial_dur / 2; 
-% 
-% par.lag_base_incl_meter_rel = [0.8]; 
-% par.lag_base_excl_meter_rel = [0.6, 1.0, 1.4]; % [0.6, 1.0, 1.4]   [2.4]
-% 
-% par.lag_base_incl_meter_unrel = [0.6, 1.0, 1.4]; % [0.6, 1.0, 1.4]   [0.2]
-% par.lag_base_excl_meter_unrel = [0.8]; 
-% 
-% [par.lags_meter_rel, par.lags_meter_unrel] = get_meter_lags(...
-%             par.max_lag, ...
-%             par.lag_base_incl_meter_rel, par.lag_base_excl_meter_rel, ...
-%             par.lag_base_incl_meter_unrel, par.lag_base_excl_meter_unrel ...
-%             );
-%        
-% % take only firsst 11 lags
-% lags = [par.lags_meter_rel, par.lags_meter_unrel]; 
-% 
-% mask_meter_rel = zeros(1, length(lags), 'logical'); 
-% mask_meter_rel(1 : length(par.lags_meter_rel)) = 1; 
-% 
-% [l, idx] = sort(lags); 
-% idx = idx(1 : length(par.frex)); 
-% 
-% par.lags_meter_rel = lags( idx(ismember(idx, find(mask_meter_rel))) ); 
-% par.lags_meter_unrel = lags( idx(ismember(idx, find(~mask_meter_rel))) ); 
-%         
-% % run only what we need
-% par.n_rep = 50; 
-% 
-% main_noiseEffectZscore_ACFvsFFT(par, ...
-%     'prepared_noise', noise_all_samples);
-% 
-% main_noiseEffectDist_ACFvsFFT(par, ...
-%     'prepared_noise', noise_all_samples);
+% This one is only to double check that the better robustnesss of ACF to noise
+% is not simply because we're taking more values (lags of interest). So let's
+% equalize the number of frex and lags of interest and re-run only the relevant
+% scripts. 
+
+sel_name = 'maxlag-11lags_meterRel-0.8_meterUnrel-0.6_1.0_1.4'; 
+
+par = get_par(); 
+
+par.data_path = fullfile(par.data_path, sel_name); 
+par.fig_path = par.data_path; 
+mkdir(par.data_path); 
+
+% frequencies of interst
+par.max_freq = 5; 
+par.max_freq_plot = 5.1; 
+par.f0_to_excl = 5; 
+[par.freq_meter_rel, par.freq_meter_unrel, par.frex] = get_meter_freq(...
+                                                par.max_freq, ...
+                                                'f0_to_excl', par.f0_to_excl);
+
+% lags of interest 
+par.max_lag = par.trial_dur / 2; 
+
+par.lag_base_incl_meter_rel = [0.8]; 
+par.lag_base_excl_meter_rel = [0.6, 1.0, 1.4]; % [0.6, 1.0, 1.4]   [2.4]
+
+par.lag_base_incl_meter_unrel = [0.6, 1.0, 1.4]; % [0.6, 1.0, 1.4]   [0.2]
+par.lag_base_excl_meter_unrel = [0.8]; 
+
+[par.lags_meter_rel, par.lags_meter_unrel] = get_meter_lags(...
+            par.max_lag, ...
+            par.lag_base_incl_meter_rel, par.lag_base_excl_meter_rel, ...
+            par.lag_base_incl_meter_unrel, par.lag_base_excl_meter_unrel ...
+            );
+       
+% take only firsst 11 lags
+lags = [par.lags_meter_rel, par.lags_meter_unrel]; 
+
+mask_meter_rel = zeros(1, length(lags), 'logical'); 
+mask_meter_rel(1 : length(par.lags_meter_rel)) = 1; 
+
+[l, idx] = sort(lags); 
+idx = idx(1 : length(par.frex)); 
+
+par.lags_meter_rel = lags( idx(ismember(idx, find(mask_meter_rel))) ); 
+par.lags_meter_unrel = lags( idx(ismember(idx, find(~mask_meter_rel))) ); 
+        
+% run only what we need
+par.n_rep = 50; 
+
+main_noiseEffectZscore_ACFvsFFT(par, ...
+    'prepared_noise', noise_all_samples);
+
+main_noiseEffectDist_ACFvsFFT(par, ...
+    'prepared_noise', noise_all_samples);
 
 
 
--- run_mains.m
+++ run_mains.m
@@ -1,56 +1,56 @@
 
 
-par.ir_type = 'square'; 
+par.ir_type = 'erp2'; 
 par.ir = get_ir(par.ir_type, par.fs); 
 
-% main_ir(par);
-% 
-% main_emph(par);
-% 
-% main_coch(par);
-% 
-% main_jitter(par);
-% 
+main_ir(par);
+
+main_emph(par);
+
+main_coch(par);
+
+main_jitter(par);
+
 
 
 %% effect of noise 
 
 par.n_rep = 50; 
 
-% 
-% main_snr(par,...
-%     'prepared_noise', noise_all_samples);
-% 
-% main_emph_vs_noise(par, ...
-%     'prepared_noise', noise_all_samples);
-% 
-% 
-% 
-% main_noiseEffectZscore_ACFvsFFT(par, ...
-%     'prepared_noise', noise_all_samples);
-% 
-% 
-% main_noiseEffectDist_ACFvsFFT(par, ...
-%     'prepared_noise', noise_all_samples);
-% 
+
+main_snr(par,...
+    'prepared_noise', noise_all_samples);
+
+main_emph_vs_noise(par, ...
+    'prepared_noise', noise_all_samples);
+
+
+
+main_noiseEffectZscore_ACFvsFFT(par, ...
+    'prepared_noise', noise_all_samples);
+
+
+main_noiseEffectDist_ACFvsFFT(par, ...
+    'prepared_noise', noise_all_samples);
+
 
 main_noiseEffectDist_allVsOnlyHarm(par, ...
     'prepared_noise', noise_all_samples); 
-% 
-% 
-% 
-% main_fooof_irasa(par,...
-%     'prepared_noise', noise_all_samples); 
-% 
-% 
+
+
+
+main_fooof_irasa(par,...
+    'prepared_noise', noise_all_samples); 
+
+
 % 
 % % need loads of samples for this one... 
 % par.n_rep = 500; 
 % 
 % main_only_noise(par,...
 %     'prepared_noise', noise_all_samples); 
-% 
-% 
+
+
 
 %% real EEG and tapping data 
 
