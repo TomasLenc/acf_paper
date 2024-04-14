57e18833293d0041023a29fafb23741fe2444be1
tomo <tomas.lenc@gmail.com>
2024-04-14 18:51:49 +0200

    move R scripts to subfolder

--- analysis.R
+++ analysis.R
@@ -1,14 +1,14 @@
 rm(list=ls())
 
 # get general parameters
-source('config.R')
+source('lib/R/config.R')
 
 data_dirs <- c(
-    'maxlag-2.4_meterRel-0.8_meterUnrel-0.6_1.0_1.4_ignore-0.4_zeroOut-false_keepBand-false', 
-    'maxlag-2.4_meterRel-0.8_meterUnrel-0.6_1.0_1.4_ignore-0.4_zeroOut-true_keepBand-false', 
-    'maxlag-2.4_meterRel-0.8_meterUnrel-0.6_1.0_1.4_ignore-0.4_zeroOut-true_keepBand-true', 
-    'maxlag-halfTrial_meterRel-0.8_meterUnrel-0.6_1.0_1.4_ignore-0.4_zeroOut-false_keepBand-false', 
-    'maxlag-halfTrial_meterRel-0.8_meterUnrel-0.6_1.0_1.4_ignore-0.4_zeroOut-true_keepBand-false', 
+    # 'maxlag-2.4_meterRel-0.8_meterUnrel-0.6_1.0_1.4_ignore-0.4_zeroOut-false_keepBand-false',
+    # 'maxlag-2.4_meterRel-0.8_meterUnrel-0.6_1.0_1.4_ignore-0.4_zeroOut-true_keepBand-false',
+    # 'maxlag-2.4_meterRel-0.8_meterUnrel-0.6_1.0_1.4_ignore-0.4_zeroOut-true_keepBand-true',
+    'maxlag-halfTrial_meterRel-0.8_meterUnrel-0.6_1.0_1.4_ignore-0.4_zeroOut-false_keepBand-false',
+    'maxlag-halfTrial_meterRel-0.8_meterUnrel-0.6_1.0_1.4_ignore-0.4_zeroOut-true_keepBand-false',
     'maxlag-halfTrial_meterRel-0.8_meterUnrel-0.6_1.0_1.4_ignore-0.4_zeroOut-true_keepBand-true'
 )
 
@@ -24,7 +24,7 @@ for (i_data_dir in c(1:length(data_dirs))) {
     }
 
     results_fname <- sprintf('report_main')
-    rmarkdown::render(sprintf('report_main.Rmd'),
+    rmarkdown::render(sprintf('lib/R/report_main.Rmd'),
                       output_file=sprintf('%s/%s.html', data_path, results_fname))
     
 
@@ -33,5 +33,5 @@ for (i_data_dir in c(1:length(data_dirs))) {
 
 # run selection-independent reports
 results_fname <- sprintf('report_sel_comparisons')
-rmarkdown::render(sprintf('report_sel_comparisons.Rmd'),
+rmarkdown::render(sprintf('lib/R/report_sel_comparisons.Rmd'),
                   output_file=sprintf('%s/data/%s.html', experiment_path, results_fname))
