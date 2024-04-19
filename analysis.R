4fa0879b43e9c6043957e174b73dddcb7ef85eee
tomo <tomas.lenc@gmail.com>
2024-04-19 11:19:23 +0200

    remove maxlag-2.4 analyses

--- analysis.R
+++ analysis.R
@@ -4,16 +4,16 @@ rm(list=ls())
 source('lib/R/config.R')
 
 data_dirs <- c(
-    'maxlag-halfTrial_meterRel-0.8_meterUnrel-0.6_1.0_1.4_ignore-0.4_zeroOut-false_keepBand-false',
-    'maxlag-halfTrial_meterRel-0.8_meterUnrel-0.6_1.0_1.4_ignore-0.4_zeroOut-true_keepBand-false',
-    'maxlag-halfTrial_meterRel-0.8_meterUnrel-0.6_1.0_1.4_ignore-0.4_zeroOut-true_keepBand-true'
+    # 'maxlag-halfTrial_meterRel-0.8_meterUnrel-0.6_1.0_1.4_ignore-0.4_zeroOut-false_keepBand-false',
+    'maxlag-halfTrial_meterRel-0.8_meterUnrel-0.6_1.0_1.4_ignore-0.4_zeroOut-true_keepBand-false'
+    # 'maxlag-halfTrial_meterRel-0.8_meterUnrel-0.6_1.0_1.4_ignore-0.4_zeroOut-true_keepBand-true'
 )
 
 i_data_dir <- 1
 
 for (i_data_dir in c(1:length(data_dirs))) {
     
-    data_path = file.path(experiment_path, 'data', data_dirs[i_data_dir])
+    data_path = file.path(experiment_path, 'results', data_dirs[i_data_dir])
     fig_path = file.path(experiment_path, 'figures', data_dirs[i_data_dir])
     
     if (! dir.exists(fig_path)) {
